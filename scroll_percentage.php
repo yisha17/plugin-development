aeb8bd23c4f69e6ca1015cb9eb6eebac20ffd4e9
Yisak <it.yisak.wondim@gmail.com>
2022-01-12 23:03:42 +0300

    information about the author

---
+++ scroll_percentage.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Page Scroll Percentage
+ * Plugin URI:        https://contactform.com
+ * Description:       A simple plugin thay shows the percentage of page scrolled
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Yishak W.
+ * Author URI:        https://fa.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ */
+
+ ?>
