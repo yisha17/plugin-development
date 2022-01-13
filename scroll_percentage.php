63a04339a6a5a9d5fcf99d542708da8b99ac902d
Yisak <it.yisak.wondim@gmail.com>
2022-01-13 09:41:05 +0300

    adding functionality

--- scroll_percentage.php
+++ scroll_percentage.php
@@ -13,4 +13,27 @@
  * Text Domain:       my-basics-plugin
  */
 
- ?>
+class PageScrollPercentage{
+
+    function __construct(){
+        add_action('admin_menu',array($this,'adminPage'));
+        add_action('admin_init',array($this,'settings'));
+    }
+
+    function settings(){
+        
+    }
+
+    function adminPage(){
+        add_options_page('page scroll setting','page_scroll','manage_options','page-scroll-setting',array($this,'addLayout'));
+    }
+
+    function addLayout(){?>
+        <div class="wrap">
+            <h1>Page Scroll Settings</h1>
+        </div>
+    <?php }
+}
+
+    $plugin = new PageScrollPercentage();
+?>
