ace8f6204bdbf5f6086e55cd793615ba1d3c418e
Yisak <it.yisak.wondim@gmail.com>
2022-01-20 19:59:47 +0300

    css an js for vertical bar

---
+++ assets/js/vertical.js
@@ -0,0 +1,19 @@
+const progressbar = document.querySelector("progress");
+const article = document.querySelector("article");
+
+let isScrolling = false;
+
+document.addEventListener("scroll", (e) => (isScrolling = true));
+
+render();
+
+function render() {
+  requestAnimationFrame(render);
+
+  if (!isScrolling) return;
+
+  progressbar.value =
+    (window.scrollY / (article.offsetHeight - window.innerHeight)) * 100;
+
+  isScrolling = false;
+}
