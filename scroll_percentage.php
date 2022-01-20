75a1a00c26b62395085c0c530f6902919edfa160
Yisak <it.yisak.wondim@gmail.com>
2022-01-20 06:06:34 +0300

    types added

--- scroll_percentage.php
+++ scroll_percentage.php
@@ -22,19 +22,39 @@ class PageScrollPercentage{
 
     function settings(){
         add_settings_section('psp_first_section','Choose Scroll Type',null,'page-scroll-setting');
+        add_settings_section('psp_second_section','choose page or post',null,'page-scroll-setting');
+
         add_settings_field('psp_type','Page Scroll Progress Type',array($this,'typeHTML'),'page-scroll-setting','psp_first_section');
-        register_setting('progressplugin','psp_type',array('sanitize_callback'=> 'sanitize_text_field','default'=> '0'));
+        register_setting('progressplugin','psp_type',array('sanitize_callback'=> array($this,'sanitizeType'),'default'=> '0'));
+
+        add_settings_field('psp_location','location',array($this,'locationHTML'),'page-scroll-setting','psp_second_section');
+        register_setting('progressplugin','psp_location',array('sanitize_callback'=> 'sanitize_text_field','default'=> '0'));
     }
 
+    function sanitizeType($input){
+
+        if($input != '0' AND $input != '1' AND $input != '2' ){
+           add_settings_error( 'psp_type', 'psp_type_error','Incorrect type of progress bar.'); 
+           return get_option('psp_type');
+        }
+        return $input;
+    }
+
+    function locationHTML(){
+        wp_list_pages();
+    }
 
     function typeHTML(){?>
-
-        <input type="radio" name="psp_type" value="0">
+        
+        <input type="radio" name="psp_type" value="0"  <?php checked(get_option('pcp_type'),'0');?>>
         <label >Vertical Linear Progress Bar</label><br>
-        <input type="radio"  name="psp_type" value="1">
+        <input type="radio"  name="psp_type" value="1" <?php checked(get_option('pcp_type'),'1') ?>>
         <label >Horizontal Linear Progress Bar</label><br>
-        <input type="radio" name="psp_type" value="2">
+        <input type="radio" name="psp_type" value="2"  <?php checked(get_option('pcp_type'),'2') ?>>
         <label >Circular Progress Bar</label>
+
+
+
     <?php }
 
     function adminPage(){
